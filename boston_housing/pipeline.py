003369ed047ceba34ab671175a415d981adea5d0
George Novack <georgenovack@Bloom.local>
2020-09-13 10:15:25 -0400

    fix train image

--- boston_housing/pipeline.py
+++ boston_housing/pipeline.py
@@ -5,22 +5,33 @@ def preprocess_op():
 
     return dsl.ContainerOp(
         name='Preprocess Data',
-        image='gnovack/mnist_pipeline_preprocessing:latest',
+        image='gnovack/boston_pipeline_preprocessing:latest',
         arguments=[],
         file_outputs={
-            'X': '/app/X.npy',
+            'x_train': '/app/X.npy',
         }
     )
 
+def train_op(x_train):
+
+    return dsl.ContainerOp(
+        name='Train Model',
+        image='gnovack/boston_pipeline_train:latest',
+        arguments=[
+            '--x_train', x_train
+        ]
+    )
+
 @dsl.pipeline(
-   name='MNIST Pipeline',
+   name='Boston Housing Pipeline',
    description='A toy pipeline that performs arithmetic calculations.'
 )
-def mnist_pipeline():
-    step = preprocess_op()
+def boston_pipeline():
+    preprocess_task = preprocess_op()
+    train_op(preprocess_task.outputs['x_train']).after(preprocess_task)
 
 client = kfp.Client()
 #Specify pipeline argument values
 arguments = {} 
 #Submit a pipeline run
-client.create_run_from_pipeline_func(mnist_pipeline, arguments=arguments)
+client.create_run_from_pipeline_func(boston_pipeline, arguments=arguments)
--- boston_housing/train/train.py
+++ boston_housing/train/train.py
@@ -4,9 +4,8 @@ def train_model(x_train):
     print(x_train)
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--x_train')
     args = parser.parse_args()
-
     train_model(args.x_train)
