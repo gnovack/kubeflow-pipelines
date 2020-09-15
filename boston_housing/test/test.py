864757a12c79dd5f3b0ffb379405ee5c4ae780c0
George Novack <georgenovack@Bloom.local>
2020-09-14 23:44:45 -0400

    write out error

--- boston_housing/test/test.py
+++ boston_housing/test/test.py
@@ -1,14 +1,20 @@
 import argparse
 import joblib
 import numpy as np
+from sklearn.metrics import mean_squared_error
 
 def test_model(x_test, y_test, model_path):
     x_test_data = np.load(x_test)
     y_test_data = np.load(y_test)
 
     model = joblib.load(model_path)
-    score = model.score(x_test_data, y_test_data)
-    print(f'R^2 score: {score}')
+    y_pred = model.predict(x_test_data)
+
+    err = mean_squared_error(y_test_data, y_pred)
+    
+    with open('output.txt', 'a') as f:
+        f.write(err)
+
 
 
 if __name__ == '__main__':
